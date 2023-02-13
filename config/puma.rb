64fcf5975e3b12a815eacdf2b4a1ca3eaaf3f075
Stas <me@stas.tk>
2023-02-13 16:23:09 +0300

    fixed slashes

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,6 @@
 root = "#{Dir.getwd}"
 
-bind "unix://#{root}/tmp/puma/socket"
+bind "unix:#{root}/tmp/puma/socket"
 pidfile "#{root}/tmp/puma/pid"
 state_path "#{root}/tmp/puma/state"
 rackup "#{root}/config.ru"
