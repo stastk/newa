03ceb66e649d804d8bb7c5c42765a41c638dd529
Stas <me@stas.tk>
2023-02-15 12:59:55 +0300

    remapper

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
 
+ARR_PHIE = %w(0 1 2 3 4 5 6 7 8 9 ¯ - « ⅄ Ⅺ ⸘ ‽ ¿ ? ! * + e b d u i k Q E w h c m n o p C r L t Y l v s y & K O A % N Z H T J a _ I B U F G z V R $ M P f S W D D g / ~ \\ ₀ ₁ ₂ ₃ ₄ ₅ ₆ ₇ ₈ ₉ ⁰ ¹ ² ³ ⁴ ⁵ ⁶ ⁷ ⁸ ⁹ ~ ­ . | ,) << " " #<< ":"
+ARR_WOTC = %w(0 1 2 3 4 5 6 7 8 9 − - « Y X ⸘ ‽ ¿ ? ! * + a b d e f g h i j k l m n o p q r s t u v w x y z ð ø č ķ ŋ θ ţ ť ž ǆ ǥ ǧ ǩ ɒ ə ɢ ɣ ɬ ɮ ɴ ʁ π φ χ ẅ ’ ' š : " / -0 -1 -2 -3 -4 -5 -6 -7 -8 -9 +0 +1 +2 +3 +4 +5 +6 +7 +8 +9 " “ . ^) << " " << "," #<< " "
+ARR_WOTC_GSUB_FROM_START = %w(¿ “ ‽ ⸘ ? ! " ^ «)
+ARR_WOTC_GSUB_FROM_END = %w(: - “ « . ^)
+
+PHIE_ALIAS = "gibbersih"
+WOTC_ALIAS = "normal"
+
 configure {
   set :server, :puma
 }
@@ -10,9 +18,60 @@ class Remapper < Sinatra::Base
     erb :index
   end
 
-  post '/' do
+  post '/check' do
     content_type :json
     { something: "#{params[:t]}" }.to_json
+  end
+
+  post '/remapper/v1' do
+    @text = params[:t] || ""
+    @direction = params[:d] || ""
+
+    remapped = ""
+
+    if @direction.to_s == "normal"
+      arr_from = ARR_WOTC
+      arr_to = ARR_PHIE
+      direction = "gibberish"
+      invert_direction = "normal"
+    else
+      arr_from = ARR_PHIE
+      arr_to = ARR_WOTC
+      direction = "normal"
+      invert_direction = "gibberish"
+    end
+
+    @text.chars.each do |char|
+      if char == "\n"
+        remapped += "\n"
+      elsif direction == "normal"
+        if ARR_PHIE.include? char
+          i = arr_from.find_index(char)
+          remapped += i.nil? ? char : arr_to[i]
+        elsif char == "\n"
+          remapped += "\n"
+        end
+      elsif direction == "gibberish"
+        if ARR_WOTC.include? char
+          i = arr_from.find_index(char)
+          remapped += i.nil? ? char : arr_to[i]
+        elsif char == "\n"
+          remapped += "\n"
+        end
+      end
+    end
+
+    space_gsubber = ->(x){x == "^" ? "\\^" : x}
+
+    ARR_WOTC_GSUB_FROM_END.each do |gg|
+      remapped.gsub!(/[#{space_gsubber.call(gg)}](\s|[,])/, "#{gg} ")
+    end
+
+    ARR_WOTC_GSUB_FROM_START.each do |gg|
+      remapped.gsub!(/(\s|[,])[#{space_gsubber.call(gg)}]/, " #{gg}")
+    end
+
+    [direction.to_s, invert_direction.to_s, remapped.to_s].to_s
   end
 
   run! if app_file == $0
