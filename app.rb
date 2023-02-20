3bc7d438ad2c9f84edcce3f23f0abf5253bdb539
Stas <me@stas.tk>
2023-02-20 23:40:13 +0300

    newlines test

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 
-ARR_PHIE = %w(0 1 2 3 4 5 6 7 8 9 ¯ - « ⅄ Ⅺ ⸘ ‽ ¿ ? ! * + e b d u i k Q E w h c m n o p C r L t Y l v s y & K O A % N Z H T J a _ I B U F G z V R $ M P f S W D D g / ~ \\ ₀ ₁ ₂ ₃ ₄ ₅ ₆ ₇ ₈ ₉ ⁰ ¹ ² ³ ⁴ ⁵ ⁶ ⁷ ⁸ ⁹ ~ ­ . | ,) << ":" << "\n"
-ARR_WOTC = %w(0 1 2 3 4 5 6 7 8 9 − - « Y X ⸘ ‽ ¿ ? ! * + a b d e f g h i j k l m n o p q r s t u v w x y z ð ø č ķ ŋ θ ţ ť ž ǆ ǥ ǧ ǩ ɒ ə ɢ ɣ ɬ ɮ ɴ ʁ π φ χ ẅ ’ ' š : " / -0 -1 -2 -3 -4 -5 -6 -7 -8 -9 +0 +1 +2 +3 +4 +5 +6 +7 +8 +9 " “ . ^) << " " << "," << "\n"
+ARR_PHIE = %w(0 1 2 3 4 5 6 7 8 9 ¯ - « ⅄ Ⅺ ⸘ ‽ ¿ ? ! * + e b d u i k Q E w h c m n o p C r L t Y l v s y & K O A % N Z H T J a _ I B U F G z V R $ M P f S W D D g / ~ \\ ₀ ₁ ₂ ₃ ₄ ₅ ₆ ₇ ₈ ₉ ⁰ ¹ ² ³ ⁴ ⁵ ⁶ ⁷ ⁸ ⁹ ~ ­ . | ,) << ":"
+ARR_WOTC = %w(0 1 2 3 4 5 6 7 8 9 − - « Y X ⸘ ‽ ¿ ? ! * + a b d e f g h i j k l m n o p q r s t u v w x y z ð ø č ķ ŋ θ ţ ť ž ǆ ǥ ǧ ǩ ɒ ə ɢ ɣ ɬ ɮ ɴ ʁ π φ χ ẅ ’ ' š : " / -0 -1 -2 -3 -4 -5 -6 -7 -8 -9 +0 +1 +2 +3 +4 +5 +6 +7 +8 +9 " “ . ^) << " " << ","
 ARR_WOTC_GSUB_FROM_START = %w(¿ “ ‽ ⸘ ? ! " ^ «)
 ARR_WOTC_GSUB_FROM_END = %w(: - “ « . ^)
 
@@ -41,21 +41,23 @@ class Remapper < Sinatra::Base
       invert_direction = "gibberish"
     end
 
+    newlines = %w(, , ,  ,  )
+
     @text.chars.each do |char|
-      if char == "\n"
+      if newlines.include? char || char == "\n"
         remapped += "\n"
       elsif direction == "normal"
-        if ARR_PHIE.include? char
+        if ARR_PHIE.include? char || char == "\n"
           i = arr_from.find_index(char)
           remapped += i.nil? ? char : arr_to[i]
-        elsif char == "\n"
+        elsif newlines.include? char
           remapped += "\n"
         end
       elsif direction == "gibberish"
         if ARR_WOTC.include? char
           i = arr_from.find_index(char)
           remapped += i.nil? ? char : arr_to[i]
-        elsif char == "\n"
+        elsif newlines.include? char || char == "\n"
           remapped += "\n"
         end
       end
